193f17c155dda29870d421d1d727c3176b128e85
Gopher Kaseya <20kw174@esisalama.org>
2024-12-17 14:37:06 +0100

    --

--- routes/web.php
+++ routes/web.php
@@ -9,5 +9,5 @@ Route::resource('produit',\App\Http\Controllers\ProduitController::class);
 Route::get('/produit-destroy/{produit}',[\App\Http\Controllers\ProduitController::class,'destroy']);
 
 Route::resource('vente',\App\Http\Controllers\VenteController::class);
-Route::get('/vente/statistiques',[\App\Http\Controllers\VenteController::class,'statistiques']);
+Route::get('/vente/statistiques',[\App\Http\Controllers\VenteController::class,'statistiques'])->name('vente.statistiques');
 
